37612f3e3365034c27b88dba85b8aa00e2ef378e
abderrahmane <macbook@MacBook-Pro-de-macbook-2.local>
2023-10-10 23:55:51 +0100

    Initialize TaskTracker App

---
+++ Colors.js
@@ -0,0 +1,15 @@
+export default colors = {   
+    red: '#FF0000',
+    green: '#00FF00',
+    yellow: '#FFFF00',
+    purple: '#800080',
+    orange: '#FFA500',
+    gray: '#808080',
+    pink: '#FFC0CB',
+    teal: '#008080',
+    brown: '#A52A2A',
+    lavender: '#E6E6FA',
+    black: '#2D3436',
+    blue: '#24A6D9',
+    white: '#FFFFFF',
+};
