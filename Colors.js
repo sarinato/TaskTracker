44c3dec73df03bfd8f09fa655330958e7ee78d5b
abderrahmane <macbook@MacBook-Pro-de-macbook-2.local>
2023-10-11 17:58:36 +0100

    TaskTracker Home page with implemtation of of Modal

--- Colors.js
+++ Colors.js
@@ -12,4 +12,5 @@ export default colors = {
     black: '#2D3436',
     blue: '#24A6D9',
     white: '#FFFFFF',
+    lightBlue: '#A7CBD9'
 };
---
+++ tempData.js
@@ -0,0 +1,55 @@
+const todoLists = [
+    {
+      name: 'Work',
+      color: '#3498db',
+      todos: [
+        { title: 'Complete project proposal', completed: false },
+        { title: 'Prepare for the meeting', completed: true },
+        { title: 'Review code changes', completed: false },
+        { title: 'Send progress report', completed: false },
+      ],
+    },
+    {
+      name: 'Personal',
+      color: '#2ecc71',
+      todos: [
+        { title: 'Go for a run', completed: true },
+        { title: 'Read a book', completed: false },
+        { title: 'Grocery shopping', completed: false },
+        { title: 'Write in the journal', completed: true },
+      ],
+    },
+    {
+        name: 'Job',
+        color: '#2ecc71',
+        todos: [
+          { title: 'Go for a run', completed: true },
+          { title: 'Read a book', completed: false },
+          { title: 'Grocery shopping', completed: false },
+          { title: 'Write in the journal', completed: true },
+        ],
+      },
+      {
+        name: 'Interview',
+        color: '#2ecc71',
+        todos: [
+          { title: 'Go for a run', completed: true },
+          { title: 'Read a book', completed: false },
+          { title: 'Grocery shopping', completed: false },
+          { title: 'Write in the journal', completed: true },
+        ],
+      },
+      {
+        name: 'Hike',
+        color: '#2ecc71',
+        todos: [
+          { title: 'Go for a run', completed: true },
+          { title: 'Read a book', completed: false },
+          { title: 'Grocery shopping', completed: false },
+          { title: 'Write in the journal', completed: true },
+        ],
+      },    
+  ];
+  
+  export default todoLists;
+  
